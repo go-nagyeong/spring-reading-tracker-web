33b35549160d798009de018ac1c05a50c7da8706
Nagyeong Kim <ngkim0929@naver.com>
2024-07-09 05:54:24 +0900

    fix: 로그아웃 get 요청 막기

--- src/main/java/com/readingtracker/boochive/config/WebSecurityConfig.java
+++ src/main/java/com/readingtracker/boochive/config/WebSecurityConfig.java
@@ -9,12 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -38,17 +38,19 @@ public class WebSecurityConfig {
                 .sessionManagement((sessionManagement) ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                // jwt 토큰 기반의 인증 처리 (커스텀 필터)
+                // jwt 토큰 기반의 인증(로그인) 처리
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 // url 접근 권한 설정
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/assets/**","/","/login","/register","/find-password","/api/auth/**").permitAll() // 권한 허가
+                        .requestMatchers("/assets/**","/","/login","/register","/find-password","/api/auth/**","/error").permitAll() // 권한 허가
                         .anyRequest().authenticated() // 그 외 모든 경로 인증 필요
-//                        .anyRequest().permitAll() // TODO: 임시
                 )
+                // 로그아웃
                 .logout(logout ->
-                        logout.clearAuthentication(true)
-                                .deleteCookies("access_token")
+                        logout.logoutRequestMatcher(new AntPathRequestMatcher("/api/auth/logout", "POST"))
+                                .logoutSuccessUrl("/login")
+                                .clearAuthentication(true)
+                                .deleteCookies("JSESSIONID","access_token","refresh_token") // TODO: (임시) 추후 Refresh Token은 Redis에 저장
                 );
 
         return http.build();
